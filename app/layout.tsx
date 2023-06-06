bdea8db1687358529840ded0321ecdd2eb844249
fayzullo2004 <fayzulloakmalov1@gmail.com>
2023-06-07 01:17:26 +0500

    create project and auth UI and MongoDB Prisma setUp

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import React from "react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Messenger',
+  description: 'Messenger',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+        suppressHydrationWarning={true}
+      >
+      {children}
+      </body>
+    </html>
+  )
+}
